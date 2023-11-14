6f3ffa84a1aba96b4be81cf4a26a0ec74a2b19a4
johnnykim22 <johnnykim22@gmail.com>
2023-11-13 17:56:13 -0800

    final finished

--- src/App.js
+++ src/App.js
@@ -1,13 +1,25 @@
-import React from 'react';
-import './App.css';
+import React from "react";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import ProductListing from "./containers/ProductListing";
+import Header from "./containers/Header";
+import "./App.css";
+import ProductDetails from "./containers/ProductDetails";
+import 'semantic-ui-css/semantic.min.css';
+
 
 function App() {
   return (
     <div className="App">
-      Async Redux Project
+      <Router>
+        <Header />
+        <Switch>
+          <Route path="/" exact component={ProductListing} />
+          <Route path="/product/:productId" component={ProductDetails} />
+          <Route>404 Not Found!</Route>
+        </Switch>
+      </Router>
     </div>
   );
 }
 
 export default App;
-
